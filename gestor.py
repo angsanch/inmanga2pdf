1fe52d6ef70dff6400c7a7d14dc8a432d13faae7
Sagitario25 <adanielsanchezgarcia@gmail.com>
2022-07-15 01:41:02 +0200

    Inital files

---
+++ gestor.py
@@ -0,0 +1,26 @@
+import inmanga
+import requests
+import img2pdf
+import os
+
+inm = inmanga.inmangaAPI ()
+
+def download_chapter (manga_url, chapter, dirpath = "", filename = None):
+	inm.select_manga (manga_url, chapter)
+
+	if filename == None: filename = f"{inm.manga_name}-{chapter}.pdf"
+
+	url_list = inm.get_chapter_image_urls ()
+	img_list = []
+	for i in range (len (url_list)):
+		img_list.append (requests.get (url_list[i]).content)
+
+	open (os.path.join (dirpath, filename), "wb").write (img2pdf.convert(img_list))
+
+def download_manga (manga_url, path = None):
+	inm.select_manga (manga_url)
+	if path == None: path = inm.manga_name
+	if not os.path.exists (path): os.mkdir (path)
+
+	for i in inm.chapterTitleList:
+		download_chapter (manga_url, i, path)
---
+++ inmanga.py
@@ -0,0 +1,43 @@
+import selenium.webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import selenium.webdriver.support.expected_conditions as EC
+
+class inmangaAPI:
+	def __init__ (self):
+		self.options = selenium.webdriver.FirefoxOptions ()
+		self.options.add_argument ("--headless")
+
+		self.driverpath = "geckodriver.exe"
+		self.binarypath = FirefoxBinary ("Firefox/firefox.exe")
+		self.driver = selenium.webdriver.Firefox (executable_path=self.driverpath, options=self.options, firefox_binary=self.binarypath)
+		self.driver.set_window_size (1200, 600)
+
+	def select_manga (self, url, chapter = None):
+		self.driver.get (url)
+		WebDriverWait (self.driver, 10).until (EC.presence_of_element_located ((By.CLASS_NAME, "select2-selection__arrow"))).click ()
+		self.chapterList = self.driver.find_element (By.ID, "select2-ChapList-results").find_elements (By.CSS_SELECTOR, "*")
+		self.chapterTitleList = [i.get_attribute ("innerHTML") for i in self.chapterList]
+		if chapter != None:
+			self.chapterList[self.chapterTitleList.index (chapter)].click ()
+		
+		self.manga_name = WebDriverWait (self.driver, 10).until (EC.presence_of_element_located ((By.TAG_NAME, "strong"))).find_element (By.TAG_NAME, "a").get_attribute ("innerHTML")
+		self.manga_name = self.manga_name [self.manga_name.rfind (">")+2:-21]
+
+		max_tabs (self.driver, 1)
+
+	def get_chapter_image_urls (self):
+		self.pageList = WebDriverWait (self.driver, 10).until (EC.presence_of_element_located ((By.ID, "PageList"))).find_elements (By.CSS_SELECTOR, "*")
+		self.baseURL = self.driver.find_elements (By.TAG_NAME, "img")[1].get_attribute ("src")
+		self.baseURL = self.baseURL[:self.baseURL.rfind("/")]
+		self.baseURL = self.baseURL[:self.baseURL.rfind("/")]
+
+		return [f"{self.baseURL}/{i.get_attribute ('innerHTML')}/{i.get_attribute ('value')}" for i in self.pageList]
+
+def max_tabs (driver, max):
+	if len (driver.window_handles) > max:
+		old_tab = driver.current_window_handle
+		driver.switch_to.window(driver.window_handles[max])
+		driver.close ()
+		driver.switch_to.window(old_tab)
