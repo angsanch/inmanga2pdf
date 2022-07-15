78b03f4eb57e5df8b6352b40298e8014eb8c3ccc
Sagitario25 <adanielsanchezgarcia@gmail.com>
2022-07-15 02:44:53 +0200

    Specify parent dir path in download_manga

--- gestor.py
+++ gestor.py
@@ -17,9 +17,10 @@ def download_chapter (manga_url, chapter, dirpath = "", filename = None):
 
 	open (os.path.join (dirpath, filename), "wb").write (img2pdf.convert(img_list))
 
-def download_manga (manga_url, path = None):
+def download_manga (manga_url, parentdir = "", dir = None):
 	inm.select_manga (manga_url)
-	if path == None: path = inm.manga_name
+	if dir == None: dir = inm.manga_name
+	path = os.path.join (parentdir, dir)
 	if not os.path.exists (path): os.mkdir (path)
 
 	for i in inm.chapterTitleList:
