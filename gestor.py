9205d78dd40ec8b5d54d1a35fd0a63250f8763c8
Sagitario25 <adanielsanchezgarcia@gmail.com>
2022-08-05 23:45:22 +0200

    Popups to select paths

--- gestor.py
+++ gestor.py
@@ -5,23 +5,17 @@ import os
 
 inm = inmanga.inmangaAPI ()
 
-def download_chapter (manga_url, chapter, dirpath = "", filename = None):
+def download_chapter (manga_url, chapter, filename):
 	inm.select_manga (manga_url, chapter)
-
-	if filename == None: filename = f"{inm.manga_name}-{chapter}.pdf"
 
 	url_list = inm.get_chapter_image_urls ()
 	img_list = []
 	for i in range (len (url_list)):
 		img_list.append (requests.get (url_list[i]).content)
 
-	open (os.path.join (dirpath, filename), "wb").write (img2pdf.convert(img_list))
+	open (filename, "wb").write (img2pdf.convert(img_list))
 
-def download_manga (manga_url, parentdir = "", dir = None):
+def download_manga (manga_url, dir):
 	inm.select_manga (manga_url)
-	if dir == None: dir = inm.manga_name
-	path = os.path.join (parentdir, dir)
-	if not os.path.exists (path): os.mkdir (path)
-
 	for i in inm.chapterTitleList:
-		download_chapter (manga_url, i, path)
+		download_chapter (manga_url, i, os.path.join (dir, 	f"{inm.manga_name}-{i}.pdf"))
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import gestor
+import tkinter
+import tkinter.filedialog
+
+root = tkinter.Tk () 
+root.withdraw ()
+root.attributes ('-topmost', True)
 
 def command_line ():
 	ans = input ("Download just one episode(1) or full manga(2): ")
 	if ans == "1":
 		manga_url = input ("Manga URL: ")
 		chapter = input ("Chapter name: ")
-		dirpath = input ("In what folder do you want to store the pdf?: ")
-		filename = input ("File name:") + ".pdf"
-		gestor.download_chapter (manga_url, chapter, dirpath, filename)
+		filename = tkinter.filedialog.asksaveasfilename (initialfile=f"Chapter {chapter}.pdf", filetypes=[("PDF file", "*.pdf")], title="Select where to save the episode")
+		gestor.download_chapter (manga_url, chapter, filename)
 	elif ans == "2":
 		manga_url = input ("Manga URL: ")
-		parentdir = input ("In what folder do you want to store the pdf?: ")
-		dir = input ("File name:")
-		gestor.download_manga (manga_url, parentdir, dir)
+		dir = tkinter.filedialog.askdirectory (title="Select the folder where all the manga will be stored")
+		gestor.download_manga (manga_url, dir)
 
 
 if __name__ == "__main__":
